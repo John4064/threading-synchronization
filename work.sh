5fd83168b5aecc62cc24e723e468d1b9fa88f050
John Parkhurst <jparkhurst120@gmail.com>
2021-11-16 12:18:14 -0500

    Auto stash before merge of "parkhurst-threads" and "origin/parkhurst-threads"

--- work.sh
+++ work.sh
@@ -3,7 +3,8 @@
 
 
 #Linux
-g++ -Wall main.cpp gl_frontEnd.cpp imageIO_TGA.cpp -lm -lGL -lglut -o focus
+g++ -Wall main.cpp gl_frontEnd.cpp imageIO_TGA.cpp rasterImage.cpp -lm -lGL -lglut -o focus
+./focus
 #Mac
 #g++ main.cpp gl_frontEnd.cpp imageIO_TGA.cpp -lm -framework OpenGL -framework GLUT -o focus
 echo "End"
